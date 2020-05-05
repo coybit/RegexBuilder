594f97a2996c81728adf21bdf755d0cae8724d4c
Mohsen Alijanpour <mohsen@getaround.com>
2020-05-05 16:51:20 +0200

    Add the basic functionalities

--- Sources/RegexBuilder/RegexBuilder.swift
+++ Sources/RegexBuilder/RegexBuilder.swift
@@ -1,4 +1,145 @@
 import Foundation
 
-struct RegexBuilder {
+class RegexBuilder {
+    private(set) var regex: String = ""
+    
+    func digit(_ quantity: Quantity, of type: Digit) -> Self {
+        switch type {
+        case .zeroToNine:
+            regex.append("\\d")
+        case .just(let digits):
+            regex.append("[\(digits.map(String.init).joined())]")
+        }
+        
+        regex.append(quantity.representation)
+        return self
+    }
+    
+    func character(_ quantity: Quantity, of types: [Character]) -> Self {
+        
+        let combined = Array<Character>(Set<Character>(types))
+            .map { $0.rawValue }
+            .sorted()
+            .joined()
+        
+        if combined == "\\p{Ll}\\p{Lo}\\p{Lu}\\p{Nd}" {
+            regex.append("\\w")
+        } else if types.count > 1 {
+            regex.append("[\(combined)]")
+        } else {
+            regex.append(combined)
+        }
+
+        regex.append(quantity.representation)
+        return self
+    }
+    
+    func space(_ quantity: Quantity, of types: [Spaces]) -> Self {
+        let combined = types
+            .map { $0.rawValue }
+            .sorted()
+            .joined()
+        
+        if combined == "\\f\\n\\p{Z}\\r\\t" {
+            regex.append("\\s")
+        } else if types.count > 1 {
+            regex.append("[\(combined)]")
+        } else {
+            regex.append(combined)
+        }
+        
+        regex.append(quantity.representation)
+        return self
+    }
+    
+    func either(_ a: RegexBuilder, _ b: RegexBuilder) -> Self {
+        regex.append(a.regex)
+        regex.append("|")
+        regex.append(b.regex)
+        return self
+    }
+    
+    func beginning(of relativeTo: Anchor) -> Self {
+        switch relativeTo {
+        case .line: regex.append("^")
+        }
+        return self
+    }
+    
+    func end(of relativeTo: Anchor) -> Self {
+        switch relativeTo {
+        case .line: regex.append("$")
+        }
+        return self
+    }
+    
+    func not(_ a: RegexBuilder) -> Self {
+        regex.append("[^\(a.regex)]")
+        return self
+    }
+    
+    enum Anchor {
+        case line
+    }
+    
+    enum Digit {
+        case zeroToNine
+        case just([Int])
+    }
+    
+    enum Spaces: String {
+        case space = "\\p{Z}"
+        case tab = "\\t"
+        case newline = "\\n"
+        case carriageReturn = "\\r"
+        case formFeed = "\\f"
+    }
+    
+    enum Character: RawRepresentable, Hashable {
+        case digit
+        case underscore
+        case dash
+        case lowerCaseAscii
+        case upperCaseAscii
+        case ideograph
+        case dot
+        case literal(Swift.Character)
+        
+        init?(rawValue: String) {
+            return nil
+        }
+        
+        var rawValue: String {
+            switch self {
+            case .digit: return "\\p{Nd}"
+            case .underscore: return "_"
+            case .dash: return "\\-"
+            case .lowerCaseAscii: return "\\p{Ll}"
+            case .upperCaseAscii: return "\\p{Lu}"
+            case .ideograph: return "\\p{Lo}"
+            case .dot: return "\\."
+            case .literal(let c): return String(c)
+            }
+        }
+    }
+    
+    enum Quantity {
+        case oneOrMore
+        case zeroOrMore
+        case exactly(Int)
+        case between(Int, Int)
+        case atLeast(Int)
+        case atMostOne
+        
+        var representation: String {
+            switch self {
+            case .oneOrMore: return "+"
+            case .zeroOrMore: return "*"
+            case .exactly(let num): return "{\(num)}"
+            case .between(let a, let b): return "{\(a),\(b)}"
+            case .atLeast(let a): return "{\(a),}"
+            case .atMostOne: return "?"
+            }
+        }
+    }
 }
--- Tests/RegexBuilderTests/RegexBuilderTests.swift
+++ Tests/RegexBuilderTests/RegexBuilderTests.swift
@@ -2,11 +2,109 @@ import XCTest
 @testable import RegexBuilder
 
 final class RegexBuilderTests: XCTestCase {
-    func testExample() {
-        XCTAssertEqual(RegexBuilder().text, "Hello, World!")
+    func test_simple_digit() {
+        let r = RegexBuilder()
+            .digit(.oneOrMore, of: .zeroToNine)
+        XCTAssertEqual(r.regex, "\\d+")
     }
 
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+    func test_simple_character() {
+        let r = RegexBuilder()
+            .character(.zeroOrMore, of: [.lowerCaseAscii, .digit, .upperCaseAscii, .ideograph])
+        XCTAssertEqual(r.regex, "\\w*")
+    }
+    
+    func test_simple_space() {
+        let r = RegexBuilder()
+            .space(.exactly(1), of: [.space, .tab, .newline, .carriageReturn, .formFeed])
+        XCTAssertEqual(r.regex, "\\s{1}")
+    }
+    
+    func test_subset_space() {
+        let r = RegexBuilder()
+            .space(.exactly(1), of: [.space, .tab])
+        XCTAssertEqual(r.regex, "[\\p{Z}\\t]{1}")
+    }
+    
+    func test_composite_digit_space_character() {
+        let r = RegexBuilder()
+            .space(.exactly(1), of: [.space, .tab, .newline, .carriageReturn, .formFeed])
+            .character(.zeroOrMore, of: [.lowerCaseAscii, .upperCaseAscii])
+            .digit(.between(2, 4), of: .just([1,2,3]))
+        XCTAssertEqual(r.regex, "\\s{1}[\\p{Ll}\\p{Lu}]*[123]{2,4}")
+    }
+    
+    func test_simple_either() {
+        let r = RegexBuilder()
+            .either(
+                RegexBuilder()
+                    .digit(.exactly(1), of: .zeroToNine),
+                RegexBuilder()
+                    .space(.atMostOne, of: [.newline])
+        )
+        XCTAssertEqual(r.regex, "\\d{1}|\\n?")
+    }
+    
+    func test_simple_not() {
+        let r = RegexBuilder()
+            .not(RegexBuilder()
+                .space(.oneOrMore, of: [.newline])
+        )
+        XCTAssertEqual(r.regex, "[^\\n+]")
+    }
+    
+    func test_email() {
+        let r = RegexBuilder()
+            .beginning(of: .line)
+            .character(.oneOrMore, of: [
+                .lowerCaseAscii,
+                .upperCaseAscii,
+                .digit,
+                .underscore,
+                .dash,
+                .dot
+            ])
+            .character(.exactly(1), of: [.literal("@")])
+            .character(.oneOrMore, of: [
+                .lowerCaseAscii,
+                .upperCaseAscii,
+                .digit,
+                .underscore,
+                .dash,
+                .dot
+            ])
+            .character(.exactly(1), of: [.dot])
+            .character(.between(2, 5), of: [
+                .lowerCaseAscii,
+                .upperCaseAscii,
+            ])
+            .end(of: .line)
+        
+        XCTAssertEqual(r.regex, #"^[\-\.\p{Ll}\p{Lu}\p{Nd}_]+@{1}[\-\.\p{Ll}\p{Lu}\p{Nd}_]+\.{1}[\p{Ll}\p{Lu}]{2,5}$"#)
+        XCTAssertEqual(matches(for: r.regex, in: "aa.com").count, 0)
+        XCTAssertEqual(matches(for: r.regex, in: "aa.com@").count, 0)
+        XCTAssertEqual(matches(for: r.regex, in: "aa@@a.com").count, 0)
+        XCTAssertEqual(matches(for: r.regex, in: "aacom").count, 0)
+        XCTAssertGreaterThan(matches(for: r.regex, in: "a_1.23-home@a_b.uss").count, 0)
+        XCTAssertGreaterThan(matches(for: r.regex, in: "test@a.com").count, 0)
+        XCTAssertGreaterThan(matches(for: r.regex, in: ".@a.test").count, 0)
+    }
+    
+//    static var allTests = [
+//        ("testExample", testExample),
+//    ]
+    
+    private func matches(for regex: String, in text: String) -> [String] {
+        do {
+            let regex = try NSRegularExpression(pattern: regex)
+            let results = regex.matches(in: text,
+                                        range: NSRange(text.startIndex..., in: text))
+            return results.map {
+                String(text[Range($0.range, in: text)!])
+            }
+        } catch let error {
+            print("invalid regex: \(error.localizedDescription)")
+            return []
+        }
+    }
 }
